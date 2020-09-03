3f7b5333bbc843a834d53a76c91256ea75809afe
James Lemire <webdev.jameslemire@gmail.com>
2020-09-02 21:10:16 -0400

    v1.0 ready to go

---
+++ index.js
@@ -0,0 +1,87 @@
+require("dotenv").config({ path: `${__dirname}/.env` });
+const Discord = require("discord.js");
+const axios = require("axios");
+const TARGETS = require("./targets.json");
+
+// Use an object for instant lookup
+const alreadySent = {};
+
+const generateLatestUrl = (subreddit, count = 1) =>
+  `https://www.reddit.com/r/${subreddit}/top/.json?count=${count}`;
+
+const generateHotUrl = (subreddit, count = 1) =>
+  `https://www.reddit.com/r/${subreddit}/hot/.json?count=${count}`;
+
+// env variables
+const {
+  NODE_ENV,
+  SERVER_PORT,
+  DISCORD_TOKEN,
+  BOT_ON,
+  channelName,
+} = process.env;
+
+// Discord Bot
+const bot = new Discord.Client();
+
+bot.on("ready", (e) => {
+  console.log("BOT READY");
+
+  // Subreddit Targets (softcore)
+  const botEnabled = BOT_ON === "true";
+  const timeToWaitInMs = 1000 * 60 * 30;
+
+  // The channel to send the images to
+  const channel = bot.channels.find((channelObj) => {
+    return channelObj.name === channelName;
+  });
+
+  const loopThroughTargets = () => {
+    TARGETS.forEach(async (subreddit) => {
+      const {
+        data: {
+          data: { children: hotChildren },
+        },
+      } = await axios.get(generateHotUrl(subreddit));
+
+      const hotFirstChild = hotChildren[0];
+      const hotImageUrl = hotFirstChild.data.url;
+      const permalink = hotFirstChild.data.permalink;
+
+      if (hotImageUrl && !alreadySent[hotImageUrl]) {
+        channel.send(`https://reddit.com${permalink} ${hotImageUrl}`);
+        alreadySent[hotImageUrl] = true;
+      }
+    });
+  };
+
+  // Loop through the target subreddits and grab the most recent top post
+  // Take the top post and post the image link to the target channel
+  if (botEnabled) {
+    loopThroughTargets();
+    setTimeout(loopThroughTargets, timeToWaitInMs);
+  }
+
+  const { username, id } = bot.user;
+  console.log(`Logged in as: ${username} - ${id}`);
+  console.log("****** (safe) process.env variables ******");
+  console.log("NODE_ENV:", NODE_ENV);
+  console.log("SERVER_PORT:", SERVER_PORT);
+  console.log("****** process.env variables ******");
+});
+
+bot.on("disconnect", (e) => {
+  console.log("Bot disconnected:", e);
+});
+
+async function login() {
+  try {
+    await bot.login(DISCORD_TOKEN);
+  } catch (err) {
+    console.error("The bot failed to login:", err);
+  }
+}
+
+login();
+
+module.exports = bot;
